2efd7d1e71f090da712df1583f4dbf3bfed01338
정인혜 <inhyea36@naver.com>
2019-01-09 12:06:13 +0900

    코드편집

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Web.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "aaaa", "bbbb" };
+            return new string[] { "아아아아", "우이이이" };
         }
 
         // GET api/values/5
